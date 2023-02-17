8787e08a80984e600ab2a114040b372295f595a4
ZainabBabayaro <babayarozainab@gmail.com>
2023-02-17 15:47:30 -0800

    Puts is used to output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-let main(void)
+/**
+ * puts- Used to print output string to screen
+ * main: entry point
+ * return: always 0 (success)
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
