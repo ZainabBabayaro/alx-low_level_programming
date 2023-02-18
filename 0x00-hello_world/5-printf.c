da435758c9e1d91fa726cc84fff6a4be11ff47b3
ZainabBabayaro <babayarozainab@gmail.com>
2023-02-17 16:03:00 -0800

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'printf Used to print output string and variables to screen'
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
