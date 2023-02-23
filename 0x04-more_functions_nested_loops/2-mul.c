5d6a77877c4cce5a91cfb28b8bc3fa9b5170b401
ZainabBabayaro <babayarozainab@gmail.com>
2023-02-23 08:32:08 -0800

    third

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 if c is upper and 0 if otherwise
+ * @c: character to be checked
+ * Return: 1 or 0
+ */
+
+int mul(int a, int b)
+
+{
+	int multiply = a * b;
+	_putchar(multiply + '0');
+}
