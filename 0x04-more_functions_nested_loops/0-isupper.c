70e9895e3844e179e68f64cb36632bb291d024b6
ZainabBabayaro <babayarozainab@gmail.com>
2023-02-23 08:15:09 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,16 @@
 /**
  * _isupper - return 1 if c is upper and 0 if otherwise
  * @c: character to be checked
- * Return: 1 or 0
+ * Return:1 or 0
  */
 int _isupper(int c)
 {
-        if (c >= 65 && c <= 90)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		retun (0);
+	}
 }
