0589937aef273f42f52bad15bc5329dde4713b23
ZainabBabayaro <babayarozainab@gmail.com>
2023-02-23 09:10:13 -0800

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 1 to 9
+ */
+
+void print_numbers(void)
+{
+
+	int num;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar("\n");
+}
